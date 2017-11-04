d721f88b71226dfd333bf786716c8de630eb1796
jacobramilo <32914079+jacobramilo@users.noreply.github.com>
2017-11-04 11:25:49 +1100

    Adding some draft layout

--- src/main/java/com/unliwebs/soundrecorder/MainActivity.kt
+++ src/main/java/com/unliwebs/soundrecorder/MainActivity.kt
@@ -2,11 +2,29 @@ package com.unliwebs.soundrecorder
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.support.v7.widget.Toolbar
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.app_bar)
+        setSupportActionBar(toolbar);
+
+        buttonRecord.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view) {
+            buttonRecord -> {
+                chronometer.base = SystemClock.elapsedRealtime()
+                chronometer.start()
+            }
+        }
     }
 }
