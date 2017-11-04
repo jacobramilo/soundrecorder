207336452519a5aa53e8e452c6f4633267384473
jacobramilo <32914079+jacobramilo@users.noreply.github.com>
2017-11-04 13:56:26 +1100

    adding the buttons and fixing the layout

--- src/main/java/com/unliwebs/soundrecorder/MainActivity.kt
+++ src/main/java/com/unliwebs/soundrecorder/MainActivity.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 import android.os.SystemClock
 import android.support.v7.widget.Toolbar
 import android.view.View
+import com.unliwebs.soundrecorder.widget.RecordButtonListener
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+class MainActivity : AppCompatActivity(), RecordButtonListener, View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,13 +17,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setSupportActionBar(toolbar);
 
         buttonRecord.setOnClickListener(this)
+        buttonRecord.recordingListener = this
+    }
+
+    override fun onStopRecording() {
+        chronometer.stop()
+        tvRecordingState.text = getString(R.string.tap_btn_to_start)
+    }
+
+    override fun onStartRecording() {
+        chronometer.base = SystemClock.elapsedRealtime()
+        chronometer.start()
+        tvRecordingState.text = getString(R.string.recording)
     }
 
     override fun onClick(view: View?) {
         when(view) {
             buttonRecord -> {
-                chronometer.base = SystemClock.elapsedRealtime()
-                chronometer.start()
+                buttonRecord.toggle();
             }
         }
     }
---
+++ src/main/java/com/unliwebs/soundrecorder/widget/RecordButtonListener.kt
@@ -0,0 +1,10 @@
+package com.unliwebs.soundrecorder.widget
+
+/**
+ * Created by jacobramilo on 4/11/17.
+ */
+interface RecordButtonListener {
+    fun onStopRecording()
+
+    fun onStartRecording()
+}
